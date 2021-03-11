bd7fb60be706340db967d4c20a2d9357a83d8a35
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-03-11 20:56:30 +0530

    configure laravel

--- ng4laravel/routes/web.php
+++ ng4laravel/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::any('/{any}', [AngularController::class, 'index'])->where('any','^(?!api).*$');
