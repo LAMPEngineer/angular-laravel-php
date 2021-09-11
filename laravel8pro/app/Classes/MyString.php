c82c3091e7e6b907e468e524889cbe9637337f9b
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-09-11 22:31:28 +0530

    slugify method

--- laravel8pro/app/Classes/MyString.php
+++ laravel8pro/app/Classes/MyString.php
@@ -5,6 +5,33 @@ namespace App\Classes;
 
 class MyString
 {
-	
+	// Function to convert string into slug format
+	public function slugify($text)
+	{
+		//replace non letter or digit by ~
+		$text = preg_replace('~[^\pL\d]+~u', '-', $text);
+
+		// transliterate
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+		// remove unwanted characters
+		$text = preg_replace('~[^-\w]+~','',$text);
+
+		// trim
+		$text = trim($text, '-');
+
+		// remove duplicate -
+		$text = preg_replace('~-+~', '-', $text);
+
+		// lowercase
+		$text = strtolower($text);
+
+		if(empty($text)){
+			echo 'n-a';
+		}
+
+		echo $text;
+
+	}
 	
 }
