8baf3bd6d1e54b0881af7cb3ee6e4d1825a46a19
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-09-12 22:22:56 +0530

    register service provider

--- laravel8pro/app/Providers/AppServiceProvider.php
+++ laravel8pro/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Classes\MyString;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('mylibrary', function($app){
+            return new MyString;
+        });
     }
 
     /**
