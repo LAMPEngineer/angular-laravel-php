ce499d1afc9b2c04f8c0661a8c3ca9a0920e57f5
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-11-26 15:32:15 +0530

    anonymous callable closure: use cases

--- test/core/closure.php
+++ test/core/closure.php
@@ -10,16 +10,38 @@
 echo "Anonymous, Callable & Closure<br/>\n";
 
 
+/*use case 1: callable type with normal function*/
+
+$sum = function (callable $callback, int|float ...$numbers): int|float {
+
+           return $callback(array_sum($numbers));
+        };
+
+  // normal function
+function foo($element){
+            return $element * 2;
+        }
+
+echo '<br>Sun1 = '.$sum('foo', 1, 2, 3, 4, 5)."\n";
+
+
+
+
+
+
+/*use case 2: closure type with anonymous function*/
+
 $sum = function (closure $callback, int|float ...$numbers): int|float {
 
            return $callback(array_sum($numbers));
         };
 
+// anonymous function
 $x = function ($element){
             return $element * 2;
         };
 
-echo '<br>Sun = '.$sum($x, 1, 2.4, 3, 4.2, 5)."\n";
+echo '<br>Sun2 = '.$sum($x, 1, 2.4, 3, 4.2, 5)."\n";
 
 
 
