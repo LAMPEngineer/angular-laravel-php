31a43281210cb101b4d78dc0c53f7a6554f41519
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-09 22:53:27 +0530

    test anonymous

--- test/core/t.php
+++ test/core/t.php
@@ -1,7 +1,7 @@
 <?php
     include("index.php");
 
-
+/* use case: 1*/
 $users = [['id' => 1, 'name'=> 'Alex'],['id' => 2, 'name' => 'Billy']];
 
 $ids = array_map(function ($user){
@@ -14,3 +14,14 @@ echo '<pre>';
 print_r($users);
 print_r($ids);
 echo '</pre>';
+
+
+/* use case: 2*/
+$name = 'Alex';
+
+$greet = function ($nm) {
+			return 'Hey ' . $nm;
+		};
+
+
+var_dump($greet($name));
