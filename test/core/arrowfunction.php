3a0547cacc62d4edae442f8c1790485d06697ba1
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-11 20:49:45 +0530

    arrow function with use cases

--- test/core/arrowfunction.php
+++ test/core/arrowfunction.php
@@ -46,3 +46,38 @@ echo '</pre>';
 
 
 echo $y;
+
+
+
+/* use case 3: 
+ *
+ * character with its count in a string
+ * 
+ */
+
+$string = 'alexander';
+
+$split = str_split($string);
+
+$result = array_map(fn ($char, $count) => compact('char', 'count'), array_unique($split), array_count_values($split));
+
+
+echo '<pre>', var_dump($result), '</pre>';
+
+
+
+
+
+/* use case 4: 
+ *
+ * Manipulate data from data array
+ * 
+ * make array with user ids
+ * 
+ */
+$users = [['id' => 1, 'name'=> 'Alex'],['id' => 2, 'name' => 'Billy']];
+
+$ids = array_map(fn ($user) => $user['id'], $users);
+
+
+echo '<pre>', 'Data: ', print_r($users, true), 'User ids: ' , print_r($ids, true), '</pre>';
--- test/core/t.php
+++ test/core/t.php
@@ -1,22 +1,8 @@
 <?php
     include("index.php");
 
+
 /* use case: 1*/
-$users = [['id' => 1, 'name'=> 'Alex'],['id' => 2, 'name' => 'Billy']];
-
-$ids = array_map(function ($user){
-	return $user['id'];
-
-}, $users);
-
-
-echo '<pre>';
-print_r($users);
-print_r($ids);
-echo '</pre>';
-
-
-/* use case: 2*/
 $name = 'Alex';
 
 $greet = fn () => 'Hey ' . $name;
@@ -26,16 +12,7 @@ var_dump($greet());
 
 
 
-/* use case: 3*/
 
-$string = 'alexander';
-
-$split = str_split($string);
-
-$result = array_map(fn ($char, $count) => compact('char', 'count'), array_unique($split), array_count_values($split));
-
-
-echo '<pre>', var_dump($result), '</pre>';
 
 
 
