757991790253ac4949fe66c872ccb893df924a46
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-14 21:01:45 +0530

    test const

--- test/core/t.php
+++ test/core/t.php
@@ -1,12 +1,44 @@
 <?php
-    //include("index.php");
+    include("index.php");
     
-    $name = 'Alex';
-	
-echo "Hello {$name}<br/>";
+/* Const vs. Define
+ *
+ *	Both are used to define a constant.
+ *	
+ *	Differences:
+ */
 
-?>
+/* 1. const is keyword whereas define() is a method*/
 
-<!-- Short tag to print-->
-<?= $name; ?>
+    const PI=3.14;
+  	echo 'PI: ', PI;
+
+
+	define("CONSTANT", "Hello world.");
+	echo CONSTANT;
+
+
+
+/* 2. const defines constant at compile time 
+ * whereas define() defines constant at runtime.
+ * 
+ */
+
+	$x = 10;
+
+	/*if ($x == 10) {
+	     const FOO = 'foo';    // Invalid
+	 }*/
+	 // but
+	 if ($x == 10) {
+	     define("BAR", 'bar'); // Valid
+	 }
+
+    echo 'BAR= '. BAR;
+
+
+/* 3. const be use to define class or interface constant 
+ * while we can't use define() for class or interface constant declaration
+ * 
+ */
 
