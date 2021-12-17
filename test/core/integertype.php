af5587457f737f96ff776edb4ab46ca5d97ad156
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-17 12:58:29 +0530

    integer readable format

--- test/core/integertype.php
+++ test/core/integertype.php
@@ -12,6 +12,7 @@ $x = 0b110;
 # octal        = 055
 # binary       = 0b110
 # integer max value = PHP_INT_MAX
+# readable format = 2_000_000_000
 
 echo $x, '<br/>';
 
@@ -25,5 +26,10 @@ echo '<br/>';
 $z = (int)5.98;
 
 var_dump(is_int($z));
+echo '<br/>';
 
 
+/* integer readable format < PHP 7.4 */
+$a = 2_000_000_000; // we could use underscores '_' like commas
+
+var_dump($a);
