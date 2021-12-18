fd0ac4462203fdfebdb2c5e993822e17f9c2bf38
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-18 10:20:49 +0530

    string type

---
+++ test/core/stringtype.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+    
+/* STRINGS */
+
+$firstname = 'Will';
+$lastname  = 'Smith';
+
+$name = $firstname. ' ' . $lastname;
+
+$name[15] = 'I';
+
+echo $name . '<br/>';
+
+var_dump($name);
