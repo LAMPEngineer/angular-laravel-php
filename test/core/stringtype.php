831e7b5cbbc641887ec0f667d6f7f5f2d00708f2
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-18 13:03:18 +0530

    heredoc & nowdoc

--- test/core/stringtype.php
+++ test/core/stringtype.php
@@ -14,3 +14,25 @@ $name[15] = 'I';
 echo $name . '<br/>';
 
 var_dump($name);
+
+
+$x = 1;
+$y = 2;
+# Heredoc 
+$heredoc = <<<TEXT
+Line $x
+Line $y
+Line 3
+TEXT;
+
+
+
+# Nowdoc 
+$nowdoc = <<<'TEXT'
+Line $x
+Line $y
+Line 3
+TEXT;
+
+echo '<br />',nl2br($heredoc);
+echo '<br /><br />',nl2br($nowdoc);
