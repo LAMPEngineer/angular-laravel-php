39f8a0a6f7d092fab0e5cccfe639bbaffd4bc4bd
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-19 21:20:59 +0530

    Operators

---
+++ test/core/operators.php
@@ -0,0 +1,29 @@
+<?php 
+
+  
+/* OPERATORS */
+
+# Arithmetic Operators (+ - * / % **)
+
+# Assignment Operators (= += -= *= /= %= **=)
+
+# String Operators (. .=)
+
+# Comparison Operators ()
+
+# Error Controller Operators ()
+
+# Incremental/Decrement Operators ()
+
+# Logical Operators ()
+
+# Bitwise Operators ()
+
+# Array Operators ()
+
+# Execution Operators ()
+
+# Type Operators ()
+
+# Nullsafe Operators - PHP8 ()
+#   
