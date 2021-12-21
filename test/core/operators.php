c62f565aa86defa5fd6343521f530a5c69a1716f
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-21 21:15:16 +0530

    fdiv function

--- test/core/operators.php
+++ test/core/operators.php
@@ -6,12 +6,18 @@
 # Arithmetic Operators (+ - * / % **)
 $x = 10;
 $y = 0 ;
+
 /* var_dump($x / $y); //Fatal error: Uncaught DivisionByZeroError.
  * 
- * PHP8: fdiv($x, $y) // 
+ * PHP8: fdiv($x, $y) //If $y is zero, then one of INF, -INF, 
+ *  or NAN will be returned. 
  */
 
 var_dump(fdiv($x, $y)); 
+
+$x1 = 10.5;
+$y1 = 2.9;
+
 
 # Assignment Operators (= += -= *= /= %= **=)
 
