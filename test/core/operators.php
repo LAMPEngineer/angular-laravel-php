f11763e6f96d2fde9b0477757db2a30dc70505d5
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-21 11:45:43 +0530

    DivideByZero error & fdiv function

--- test/core/operators.php
+++ test/core/operators.php
@@ -4,6 +4,14 @@
 /* OPERATORS */
 
 # Arithmetic Operators (+ - * / % **)
+$x = 10;
+$y = 0 ;
+/* var_dump($x / $y); //Fatal error: Uncaught DivisionByZeroError.
+ * 
+ * PHP8: fdiv($x, $y) // 
+ */
+
+var_dump(fdiv($x, $y)); 
 
 # Assignment Operators (= += -= *= /= %= **=)
 
