321ccfa9402478555dfbbc027b027072c14a4afd
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-22 10:57:03 +0530

    fmod - floating point reminder

--- test/core/operators.php
+++ test/core/operators.php
@@ -18,26 +18,42 @@ var_dump(fdiv($x, $y));
 $x1 = 10.5;
 $y1 = 2.9;
 
+var_dump($x1 % $y1); // return 0
+
+/* fmod() — Returns the floating point remainder (modulo) 
+ * of the division of the arguments 
+ */
+var_dump(fmod($x1, $y1)); 
 
 # Assignment Operators (= += -= *= /= %= **=)
 
+
 # String Operators (. .=)
+
 
 # Comparison Operators (== === != !== < > <= => <=> ?? ?:)
 
+
 # Error Controller Operators (@)
+
 
 # Incremental/Decrement Operators (++ --)
 
+
 # Logical Operators (&& || ! and or xor)
+
 
 # Bitwise Operators (& | ^ ~ << >>)
 
+
 # Array Operators (+ == === != <> !==)
+
 
 # Execution Operators (``)
 
+
 # Type Operators (instanceof)
+
 
 # Nullsafe Operators - PHP8 (?)
 
