96131e218096d4041f9b139354e4784aee27eeee
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-23 07:09:55 +0530

    null coalescing vs ternary

---
+++ test/core/nullcoalescing_vs_ternary_operator.php
@@ -0,0 +1,27 @@
+<?php
+
+ /* Null coalescing vs Ternary operator */
+
+/* Ternery Operator - ?: -
+ *
+ * i. ?: is like a door that lets anything falsy through - including NULL
+ *
+ * ii. Anything falsy: 0, empty string, NULL, false, !isset(), empty()
+ *
+ * iii. X ? Y : Z
+ *
+ * iv. throw PHP NOTICE on undefined undefined(unset or !isset())
+ */
+
+
+/* 
+ * Null coalescing Operator ?? -
+ * 
+ * i. 
+ *
+ * ii.
+ *
+ * iii. 
+ * 
+ */
+
