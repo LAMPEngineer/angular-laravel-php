4cc72c3e76598a989915768b8a054a59a6b4dc5f
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-27 12:38:43 +0530

    array operators

--- test/core/operators.php
+++ test/core/operators.php
@@ -116,7 +116,9 @@ $y = 3;
 echo '<br/>& = ',var_dump($x & $y); // 2
 
 # 9. Array Operators (+ == === != <> !==)
-
+$x = ['a'=> 1, 'b'=>2, 'c'=>3];
+$y = ['a'=>4, 'e'=>5, 'f'=>6, 'g'=>7, 'h'=>8];
+echo '<br/>array + = ';print_r($x + $y); // union
 
 # 10. Execution Operators (``)
 
