6ca218af0a60c4d69f70f1fa61959a3a3bd4139d
LAMPEngineer <LAMP.Engineer@gmail.com>
2021-12-27 12:49:30 +0530

    Execution Operator

--- test/core/operators.php
+++ test/core/operators.php
@@ -120,10 +120,14 @@ $x = ['a'=> 1, 'b'=>2, 'c'=>3];
 $y = ['a'=>4, 'e'=>5, 'f'=>6, 'g'=>7, 'h'=>8];
 echo '<br/>array + = ';print_r($x + $y); // union
 
+
 # 10. Execution Operators (``)
+$output = `ls -al`;
+echo '<br/>Execution Operator(``)', "<pre>$output</pre>"; // Use of the backtick operator is identical to shell_exec().
 
 
 # 11. Type Operators (instanceof)
+
 
 
 # 12. Nullsafe Operators - PHP8 (?)
