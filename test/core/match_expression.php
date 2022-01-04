f4cf8751db9d2e8b587774a027130aac460502b5
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-04 18:25:58 +0530

    match expressiong

--- test/core/match_expression.php
+++ test/core/match_expression.php
@@ -1,3 +1,30 @@
 <?php
 
 /*Match Expression*/
+
+/* The match expression branches evaluation based on 
+ * an identity check of a value(similar to switch/case).
+ *
+ */
+$food = 'cake';
+
+$reurn_value = match($food){
+	'apple' => 'This is an apple',
+	'bar' => 'This food is a bar',
+	'cake' => 'This food is a cake'
+};
+
+var_dump($reurn_value);
+
+
+/* default value: */
+
+$statusCode = 320;
+$message = match($statusCode){
+	200, 300 => null,
+	400 => 'not found',
+	500 => 'server error',
+	default => 'unknown status code', 
+};
+
+echo '<br/> status = ', var_dump($message);
