5ad9f89acd835a75be9303e9b3b7894c3cdfa35b
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-06 22:19:38 +0530

    Named Arguments

--- test/core/named_arguments.php
+++ test/core/named_arguments.php
@@ -6,3 +6,16 @@
  * based on the parameter name,rather than parameter position.
  *  
  */
+declare(strict_types=1);
+function foo(int $x, int $y): int{
+	if($x % $y === 0){
+		return $x / $y;
+	}
+
+	return $x;
+}
+
+$x = 6;
+$y = 3;
+
+echo foo(y:$y, x:$x);
