bcea475ed1ab164879c976d7e3cc6d2312e59fb4
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-07 11:32:12 +0530

    named argument properties

--- test/core/named_arguments.php
+++ test/core/named_arguments.php
@@ -18,4 +18,48 @@ function foo(int $x, int $y): int{
 $x = 6;
 $y = 3;
 
-echo foo(y:$y, x:$x);
+echo foo(y:$y, x:$x); 
+
+# Order of paremeters are not inportent
+
+# Without $ sign will be the parameter name
+
+
+
+
+/* Optional parameter & default values
+
+ * if named parameter call-site does not set a 
+ * value for the parameter, the default value will be 
+ */
+
+function greet(string $name, string $greeting = 'Hello'){
+	echo "$greeting, $name";
+}
+echo '<br/>';
+greet(name: 'Simon');
+
+
+
+
+/*Skipping parameters 
+* 
+* With named parameter, the default value is inherited 
+* if it is not set at call-site
+*/
+function greet1( string $name, string $greeting = 'Hello', bool $warm = false){
+
+	echo "$greeting, $name";
+	if($warm){
+		echo " You are awesome";
+	}
+}
+
+echo '<br/>';
+greet1(name: "Simon", warm: true);
+
+
+
+/*Refrence:
+ * https://php.watch/versions/8.0/named-parameters
+ */
