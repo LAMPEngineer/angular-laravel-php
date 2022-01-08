9fe3e06c389af92e5026dad8ed7bacf8f7b5c7bc
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-08 22:39:55 +0530

    date-time operations

--- test/core/date_time.php
+++ test/core/date_time.php
@@ -3,11 +3,30 @@
 /* Date and Time */
 
 
-echo time();
+$currentTime = time();
+
+echo 'CurrentTimeStemp      = ' . $currentTime . '<br />';
+
+echo 'CurrentTimeStemp + 5D = ' .$currentTime + 5 * 24 * 60 * 60 . '<br />';
+
+echo 'CurrentTimeStemp - 1D = ' . $currentTime - 24 * 60 * 60 . '<br />';
+
+
+echo date('d/m/Y g:ia') . '<br />';
+
+echo date('d/m/Y g:ia', $currentTime + 5 * 24 * 60 * 60) . '<br />';
+
+echo date('d/m/Y g:ia', $currentTime - 24 * 60 * 60) . '<br />';
+
+
+
+
+
+
+
 
 
 # Time zones
 # Parsing dates
 # mktime() - get Unix timestamp
 # Datetime vs. Timestamp
-# 
