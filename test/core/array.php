036693ca310e3c6f5b4dadccfe409578a142e99f
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-11 16:40:39 +0530

    array_change_key_case

--- test/core/array.php
+++ test/core/array.php
@@ -3,9 +3,36 @@
 
 /* ARRAY */
 
-/* array_chunk(array $array, int $length, bool $preserveKeys = false): array
+/*
+ * 1. array_change_key_case(array $array, int $case = CASE_LOWER): array 
+ *     - Changes the case of all keys in an array 
+ */
+$input_arr = ['FirSt' => 1, 'SecOnd' => 4];
+prettyPrintArray(array_change_key_case($input_arr, CASE_UPPER));
+
+
+
+/* 2. array_chunk(array $array, int $length, bool $preserveKeys = false): array
  *         - Split an array into chunks
  */          
 $item = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4,  'e' => 5];
 prettyPrintArray(array_chunk($item, 2, true));
 
+
+# array_combine
+# array_filter
+# array_values
+# array_filter - no callback
+# array_keys
+# array_map
+# array_merge
+# array_reduce
+# array_search
+# in_array
+# array_diff
+# array_diff_assoc
+# array_diff_keys
+# asort
+# ksort
+# usort
+# Array destructuring
