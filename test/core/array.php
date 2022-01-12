8c1aa5d0e7725817c0364ba95abc61237afedf2b
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-01-12 12:17:22 +0530

    array_column

--- test/core/array.php
+++ test/core/array.php
@@ -8,7 +8,9 @@
  *     - Changes the case of all keys in an array 
  */
 $input_arr = ['FirSt' => 1, 'SecOnd' => 4];
+echo '---- array_change_key_case ----<br />';
 prettyPrintArray(array_change_key_case($input_arr, CASE_UPPER));
+
 
 
 
@@ -16,8 +18,50 @@ prettyPrintArray(array_change_key_case($input_arr, CASE_UPPER));
  *         - Split an array into chunks
  */          
 $item = ['a' => 1, 'b' => 2, 'c' => 3, 'd' => 4,  'e' => 5];
+echo '---- array_chunk ----<br />';
 prettyPrintArray(array_chunk($item, 2, true));
 
+
+
+/*
+ * 3. array_column(array $array, 
+ *              int|string|null $column_key, 
+ *              int|string|null $index_key = null
+ *              ): array 
+ *              
+ *   - returns the values from a single column of array, identified by 
+ *     column_key. Optionally, index_key is to index the values in the 
+ *     returned array
+ */
+$records = array(
+    array(
+        'id' => 2135,
+        'first_name' => 'John',
+        'last_name' => 'Doe'
+    ),
+    array(
+        'id' => 2245,
+        'first_name' => 'Sally',
+        'last_name' => 'Smith'
+    ),
+    array(
+        'id' => 5342,
+        'first_name' => 'Jane',
+        'last_name' => 'Jones'
+    ),
+    array(
+        'id' => 5623,
+        'first_name' => 'Peter',
+        'last_name' => 'Doe'
+    ),
+);
+
+$first_names = array_column($records, 'first_name');
+echo '---- array_column ----<br />';
+prettyPrintArray($first_names);
+
+echo '---- array_column index by id ----<br />';
+prettyPrintArray(array_column($records, 'last_name', 'id'));
 
 # array_combine
 # array_filter
