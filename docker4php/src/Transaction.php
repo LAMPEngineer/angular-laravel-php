a0d7ad84270cfdec715c4ced1a7c6f4fe71683d6
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-11 21:26:19 +0530

    oop transaction

---
+++ docker4php/src/Transaction.php
@@ -0,0 +1,18 @@
+<?php
+ declare(strict_types=1);
+
+ class Transaction
+ {
+ 	public float $amount;
+ 	public string $description;
+
+ 	public function __construct(float $amount, string $description){
+ 		$this->amount = $amount;
+ 		$this->description = $description;
+ 	}
+
+ 	public function addTax(float $rate){
+ 		$this->amount += $this->amount * $rate / 100; 
+ 	}
+
+ }
--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -1,3 +1,11 @@
-# src/public/index.php
 <?php
-echo phpinfo();
+ declare(strict_types=1);
+
+ require_once '../Transaction.php';
+
+
+ // Classes & Objects
+ $transaction = new Transaction(15, 'Transaction 1');
+ $transaction->addTax(8);
+
+ var_dump($transaction->amount);
