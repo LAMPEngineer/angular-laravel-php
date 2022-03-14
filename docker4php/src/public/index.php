fd8f4007c4668cf9ddd0dce4044fb193350c7c83
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-14 15:09:11 +0530

    two transections

--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -3,13 +3,17 @@
 
  require_once '../Transaction.php';
 
- $class = 'Transaction';
 
  // Classes & Objects
- $amount = (new $class(100, 'Transaction 1'))
+ $transaction1 = (new Transaction(100, 'Transaction 1'))
             ->addTax(8)
-            ->applyDiscount(10)
-            ->getAmount();
+            ->applyDiscount(10);
 
 
- var_dump($amount);
+ $transaction2 = (new Transaction(200, 'Transaction 2'))
+            ->addTax(8)
+            ->applyDiscount(15);
+
+
+
+ var_dump($transaction1->getAmount(), $transaction2->getAmount());
