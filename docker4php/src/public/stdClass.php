0e053fef16eedcb3f4b3465351954a39835a9128
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-15 21:50:22 +0530

    Cast integer to object

--- docker4php/src/public/stdClass.php
+++ docker4php/src/public/stdClass.php
@@ -15,6 +15,7 @@
 
 
 /*PHP stdClass*/
+
 $obj = new \stdClass();
 
 $obj->p = 15;
@@ -29,6 +30,7 @@ $obj->q = 16;
 
 
 /*Cast to an Object*/
+
 $arr = [4, 5, 6];
 $obj = (object) $arr;
 echo '<br />Cast to an Object: <br />';
@@ -38,4 +40,21 @@ var_dump($obj);
  print_r($obj);
  echo "</pre>";
 
-var_dump($obj->{0});//access std class property
+echo 'access stdClass property: <br />';
+var_dump($obj->{2});//access stdClass property
+
+
+
+/*Cast other type to an Object*/
+
+echo '<br /><br />Cast integer to object:<br />';
+$cast = (object) 25;
+var_dump($cast);
+
+ echo "<pre>";
+ print_r($cast);
+ echo "</pre>";
+
+
+echo 'Access property:<br />';
+var_dump($cast->scalar);
