5b79d169498748dd8fcb190ad29364348bd9490a
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-15 21:17:00 +0530

    create custome object using stdClass

--- docker4php/src/public/stdClass.php
+++ docker4php/src/public/stdClass.php
@@ -1,13 +1,28 @@
 <?php
 
- //PHP stdClass
+ /*JSON decode returns stdClass object*/
  
  $str = '{"a":1,"b":2,"c":3}';
 
  $arr = json_decode($str);
 
+ echo 'JSON decode returns stdClass object: <br /><br />';
  var_dump($arr->c);//access std class property
 
  echo "<pre>";
  print_r($arr);
+ echo "</pre>";
+
+
+/*PHP stdClass*/
+$obj = new \stdClass();
+
+$obj->p = 15;
+$obj->q = 16;
+
+ echo '<br />PHP stdClass: <br />';
+ var_dump($obj);
+
+ echo "<pre>";
+ print_r($obj);
  echo "</pre>";
