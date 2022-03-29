7059f1b8b9b429a93445fd3dc43928f8b64dd934
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-29 11:29:23 +0530

    PHP built-in class in global namespace

--- docker4php/src/PaymentGateway/Paddle/Transaction.php
+++ docker4php/src/PaymentGateway/Paddle/Transaction.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-//namespace PaymentGateway\Paddle;
+namespace PaymentGateway\Paddle;
+
+use DateTime;
 
 class Transaction
 {
   public function __construct()
   {
-      var_dump(new CustomerProfile());
+      var_dump(new DateTime());
   }
 }
