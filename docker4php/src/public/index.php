0795332b62622f2fd2ba0dd394c4b668ea65882c
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-03-31 21:50:40 +0530

    class constatn - object level

--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -4,6 +4,6 @@ use \App\PaymentGateway\Paddle\Transaction;
 require __DIR__ . '/../vendor/autoload.php';
 
 
-//$paddleTransaction = new Transaction();
+$paddleTransaction = new Transaction();
 
-echo Transaction::STATUS_PAID;
+echo $paddleTransaction::STATUS_PAID;
