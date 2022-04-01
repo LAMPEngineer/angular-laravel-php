b0a985bdf71a7b8fe7868c0cf3b9b7b948d34b66
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-01 11:34:37 +0530

    Lookup Table to check status

--- docker4php/src/app/PaymentGateway/Paddle/Transaction.php
+++ docker4php/src/app/PaymentGateway/Paddle/Transaction.php
@@ -10,6 +10,14 @@ class Transaction
   public const STATUS_PENDING = 'pending';
   public const STATUS_DECLINED = 'declined';
 
+
+  /* Lookup table */
+  public const ALL_STATUS = [
+    self::STATUS_PAID => 'Paid',
+    self::STATUS_PENDING => 'Pending',
+    self::STATUS_DECLINED => 'Declined'
+  ];
+
   private string $status;
 
   
@@ -20,6 +28,9 @@ class Transaction
 
   public function setStatus(string $status)
   {
+    if(!isset(self::ALL_STATUS[$status])){
+      throw new \InvalidArgumentException('Invalid status');
+    }
     $this->status = $status;
 
     return $this;
