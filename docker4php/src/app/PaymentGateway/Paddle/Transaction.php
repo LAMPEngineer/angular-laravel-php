90c0bc45f2f3f5cf41411fab860684c42eba1239
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-02 16:23:32 +0530

    enums status

--- docker4php/src/app/PaymentGateway/Paddle/Transaction.php
+++ docker4php/src/app/PaymentGateway/Paddle/Transaction.php
@@ -4,31 +4,22 @@ declare(strict_types=1);
 
 namespace App\PaymentGateway\Paddle;
 
+use App\Enums\Status;
+
 class Transaction
 {
-  public const STATUS_PAID = 'paid';
-  public const STATUS_PENDING = 'pending';
-  public const STATUS_DECLINED = 'declined';
-
-
-  /* Lookup table */
-  public const ALL_STATUS = [
-    self::STATUS_PAID => 'Paid',
-    self::STATUS_PENDING => 'Pending',
-    self::STATUS_DECLINED => 'Declined'
-  ];
 
   private string $status;
 
   
   public function __construct()
   {
-      $this->setStatus(self::STATUS_PENDING);      
+      $this->setStatus(Status::PENDING);      
   }
 
   public function setStatus(string $status)
   {
-    if(!isset(self::ALL_STATUS[$status])){
+    if(!isset(Status::ALL_STATUS[$status])){
       throw new \InvalidArgumentException('Invalid status');
     }
     $this->status = $status;
--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -1,11 +1,13 @@
 <?php
-use \App\PaymentGateway\Paddle\Transaction;
+
+use App\Enums\Status;
+use App\PaymentGateway\Paddle\Transaction;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 
 $transaction = new Transaction();
 
-$transaction->setStatus(Transaction::STATUS_PAID);
+$transaction->setStatus(Status::PAID);
 
 var_dump($transaction);
