2b7ce9f2b701b6c60ea2b5452c6ee49255e46d6e
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-05 12:28:32 +0530

    static property with multiple instances

--- docker4php/src/app/PaymentGateway/Stripe/Transaction.php
+++ docker4php/src/app/PaymentGateway/Stripe/Transaction.php
@@ -6,13 +6,18 @@ namespace App\PaymentGateway\Stripe;
 
 class Transaction
 {
-    public static int $count = 0;
+    private static int $count = 0;
     
     public function __construct(
         public float $amount,
         public string $description
     ){
         self::$count++;
+    }
+
+    public static function getCount(): int
+    {
+        return self::$count;
     }
 
     public function process()
--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -7,12 +7,12 @@ use App\PaymentGateway\Stripe\Transaction;
 require __DIR__ . '/../vendor/autoload.php';
 
 
-// $transaction = new Transaction(25, 'Transaction 1');
-// $transaction = new Transaction(25, 'Transaction 1');
-// $transaction = new Transaction(25, 'Transaction 1');
-// $transaction = new Transaction(25, 'Transaction 1');
-// $transaction = new Transaction(25, 'Transaction 1');
+$transaction = new Transaction(25, 'Transaction 1');
+$transaction = new Transaction(25, 'Transaction 1');
+$transaction = new Transaction(25, 'Transaction 1');
+$transaction = new Transaction(25, 'Transaction 1');
+$transaction = new Transaction(25, 'Transaction 1');
 
 //$transaction->setStatus(Status::PAID);
 
-var_dump(Transaction::$count);
+var_dump(Transaction::getCount());
