296ca9062076e5e26df5cb43fd3d4441d5c3f9db
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-12 17:10:21 +0530

    overriden methods could have additional arguments but without default value not allowed

--- docker4php/src/app/Text.php
+++ docker4php/src/app/Text.php
@@ -4,7 +4,7 @@
 
  class Text extends Field
  {
-    public function render($x=1): string
+    public function render($x): string
     {
         return <<<HTML
             <input type="text" name="{$this->name}" />
