c90f43c73b3042de60a875532d0592d4eb95ec37
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-26 20:58:54 +0530

    __call() & __callStatic() usecases

--- docker4php/src/app/Str.php
+++ docker4php/src/app/Str.php
@@ -11,6 +11,12 @@ class Str
         'lower' => 'strtolower',
         'len'   => 'strlen'
     ];
+    
+    private $functions = [
+        'upper' => 'strtoupper',
+        'lower' => 'strtolower',
+        'len'   => 'strlen'
+    ];
 
     public static function __callStatic(string $method, array $parameters)
     {
@@ -19,6 +25,16 @@ class Str
         }   
 
         return call_user_func_array(self::$methods[$method], $parameters);
+    }
+    
+    
+    public function __call(string $method, array $parameters)
+    {
+        if(!array_key_exists($method, $this->functions)){
+            throw new Exception('The ' . $method . ' is not supproted.');
+        }   
+
+        return call_user_func_array($this->functions[$method], $parameters);
     }
 
 }
--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -4,10 +4,21 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 // $invoice = new App\Invoice();
-
 // App\Invoice::process('static call');
 
+$str = new App\Str();
 
+/* Non statis calls */
+echo ' Non statis calls: ' . PHP_EOL;
+echo $str->lower('Hello') . PHP_EOL;
+echo $str->upper('Hello') . PHP_EOL;
+echo $str->len('Hello') . PHP_EOL;
+
+
+
+/* Statis calls */
+echo ' Statis calls: ' . PHP_EOL;
 echo App\Str::lower('Hello') . PHP_EOL;
 echo App\Str::upper('Hello') . PHP_EOL;
 echo App\Str::len('Hello') . PHP_EOL;
+
