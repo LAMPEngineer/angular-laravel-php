28ae69ba0126bbc3f34c2148930e7cbde0038d79
LAMPEngineer <LAMP.Engineer@gmail.com>
2022-04-29 11:59:12 +0530

    public index file for late static binding

--- docker4php/src/public/index.php
+++ docker4php/src/public/index.php
@@ -3,16 +3,4 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// $classA = new App\ClassA();
-// $classB = new App\ClassB();
-
-// echo $classA->getName() . PHP_EOL;
-// echo $classB->getName() . PHP_EOL;
-
-
-// echo App\ClassA::getname(). PHP_EOL;
-// echo App\ClassB::getname(). PHP_EOL;
-
-var_dump(App\ClassA::make());
-var_dump(App\ClassB::make());
 
